f1cab5e271f1197dbce670830b0721ae2295d8f8
lucasres <lucasresone@gmail.com>
2022-07-18 20:49:55 -0300

    refazendo o producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,123 +1,55 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	var asyncF bool
-
-	flag.BoolVar(&asyncF, "async", false, "make mod async")
-
-	flag.Parse()
-
-	if asyncF {
-		Async()
-	} else {
-		Sync()
-	}
-
-}
-
-func NewKafkaProducer() *kafka.Producer {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers":   "kafka-estudo_kafka_1:9092",
-		"delivery.timeout.ms": "30000",
-		"acks":                "1",
-		"enable.idempotence":  "false",
-	})
-
+	ch := make(chan kafka.Event)
+	fmt.Println("Conectando...")
+	p, err := newKafkaProducer()
 	if err != nil {
-		log.Fatalf("cannot create kafka producer: %v", err)
+		panic(err)
 	}
 
-	return p
+	fmt.Println("Publicando mensagem...")
+	err = publishMensage(
+		p,
+		"order",
+		"#1",
+		`{"orderId": "1", "userId":"1", "products": [{"id": 1, "qtd": 1}]}`,
+		ch,
+	)
+	if err != nil {
+		panic(err)
+	}
+	// espera o retorno do chan
+	result := <-ch
+	fmt.Println("Resultado:")
+	fmt.Println(result.String())
 }
 
-func Publish(value, topic, key string, producer *kafka.Producer, deliveryChan chan kafka.Event) error {
-	var bKey []byte
+func newKafkaProducer() (*kafka.Producer, error) {
+	return kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka-estudo_kafka_1:9092",
+	})
+}
 
-	if key != "" {
-		bKey = []byte(key)
-	}
-
+func publishMensage(
+	producer *kafka.Producer,
+	topic, key, payload string,
+	ch chan kafka.Event,
+) error {
 	msg := &kafka.Message{
-		Value: []byte(value),
-		Key:   bKey,
+		Value: []byte(payload),
 		TopicPartition: kafka.TopicPartition{
 			Topic:     &topic,
 			Partition: kafka.PartitionAny,
 		},
+		Key: []byte(key),
 	}
 
-	err := producer.Produce(msg, deliveryChan)
-
-	return err
-}
-
-func Sync() {
-	deliveryChan := make(chan kafka.Event)
-
-	producer := NewKafkaProducer()
-
-	err := Publish(
-		"msg-01",
-		"teste",
-		"key",
-		producer,
-		deliveryChan,
-	)
-
-	if err != nil {
-		log.Fatalf("cannot publish message: %v", err)
-	}
-
-	e := <-deliveryChan
-
-	msg := e.(*kafka.Message)
-
-	if msg.TopicPartition.Error != nil {
-		log.Fatalf("cannot publish message: %v", msg.TopicPartition.Error)
-	}
-
-	fmt.Println("message published: " + msg.TopicPartition.String())
-}
-
-func Async() {
-	deliveryChan := make(chan kafka.Event)
-
-	producer := NewKafkaProducer()
-
-	err := Publish(
-		"msg-01",
-		"teste",
-		"key",
-		producer,
-		deliveryChan,
-	)
-
-	if err != nil {
-		log.Fatalf("cannot publish message: %v", err)
-	}
-
-	go DeliveryReport(deliveryChan)
-
-	producer.Flush(2000)
-}
-
-func DeliveryReport(deliveryChan chan kafka.Event) {
-	for e := range deliveryChan {
-		switch ev := e.(type) {
-		case *kafka.Message:
-			if ev.TopicPartition.Error != nil {
-				log.Fatalf("cannot publish message: %v", ev.TopicPartition.Error)
-			}
-
-			fmt.Println("message published: " + ev.TopicPartition.String())
-		}
-	}
+	return producer.Produce(msg, ch)
 }
